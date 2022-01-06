d661249b498c4ad40fd97f03b1f7b8f2af52b6ad
ryosuke <rkato5680@gmail.com>
2022-01-05 21:06:45 -0800

    整数1つをコンパイルするコンパイラを作成

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+  
+  ./9cc "$input" > tmp.s
+  docker run --rm -v /Users/katouryoushi/project/02cs_basic/compilerbook/9cc:/9cc -w /9cc compilerbook cc -o tmp tmp.s
+  docker run --rm -v /Users/katouryoushi/project/02cs_basic/compilerbook/9cc:/9cc -w /9cc compilerbook ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
